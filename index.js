44f1bc48c1edc2b9453315a1f4ec74e01255f075
vidaricompany <102556884+kent250@users.noreply.github.com>
2025-12-04 10:09:33 +0200

    Refactor about page endpoint
    Standardized string quotes to double quotes and updated the '/about' route to include additional information which is my Names. Improved code consistency across all route handlers.

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello, World!',
-        status: 'success',
-    });
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello, World!",
+    status: "success",
+  });
 });
 
-app.get('/about', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('About Page\n');
+app.get("/about", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("About Page\nMy name is Shumbusho Irumva\n");
 });
 
-app.get('/health', (req, res) => {
-    res.status(200).json({
-        status: 'UP',
-        timestamp: new Date().toISOString()
-    });
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "UP",
+    timestamp: new Date().toISOString(),
+  });
 });
 
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
